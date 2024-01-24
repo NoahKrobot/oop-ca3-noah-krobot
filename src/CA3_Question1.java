3891cb1031ba5b600c4b40a21d2912d96096e1cb
NoahKrobot <D00259927@student.dkit.ie>
2024-01-24 09:46:47 +0000

    Q1 - stacks and variables initialized

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  *  Name:
  *  Class Group:
@@ -10,6 +12,31 @@ public class CA3_Question1
     }
 
     public static void main(String[] args) {
+
         runSimulation();
+    }
+
+    public static void stackOperations(){
+        Stack<Integer> driveway = new Stack<Integer>();
+        driveway.push(1);
+        driveway.push(2);
+        driveway.push(3);
+        driveway.push(4);
+        driveway.push(5);
+
+        Stack<Integer> street = new Stack<Integer>();
+        street.push(-1);
+        street.push(-2);
+        street.push(-3);
+        street.push(-4);
+        street.push(-5);
+
+        int newVehicle = 0;
+        int topOfDriveway = 0;
+
+
+
+
+
     }
 }
