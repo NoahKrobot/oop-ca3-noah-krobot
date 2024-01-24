d2c955f99e80bf6caa0cc021b65d13ceba89d223
NoahKrobot <D00259927@student.dkit.ie>
2024-01-24 10:48:35 +0000

    Q1 - runSimulation method modified

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -7,9 +7,36 @@ import java.util.Stack;
  */
 public class CA3_Question1
 {
+    static Stack<Integer> driveway = new Stack<Integer>();
+    static Stack<Integer> street = new Stack<Integer>();
+
 
     public static void runSimulation()
     {
+        int command = 0;
+
+        boolean runWhile=true;
+
+        while(runWhile){
+           if(command ==0) {
+               runWhile=false;
+               System.out.println("Driveway: ");
+               System.out.println(driveway);
+
+               System.out.println("Street: ");
+               System.out.println(street);
+           }
+           if(command <0){
+               removeCar(command);
+           }if(command >0){
+               addCar(command);
+
+            }
+
+
+        }
+
+        System.out.println("Entering a number.");
 
     }
 
@@ -18,14 +45,12 @@ public class CA3_Question1
     }
 
     public static void stackOperations(){
-        Stack<Integer> driveway = new Stack<Integer>();
         driveway.push(1);
         driveway.push(2);
         driveway.push(3);
         driveway.push(4);
         driveway.push(5);
 
-        Stack<Integer> street = new Stack<Integer>();
         street.push(-1);
         street.push(-2);
         street.push(-3);
@@ -36,13 +61,12 @@ public class CA3_Question1
         int topOfDriveway = 0;
     }
 
-    public static void addCar(){
-        Stack<Integer> driveway = new Stack<Integer>();
+    public static void addCar(int newCar){
         Scanner key = new Scanner(System.in);
         boolean runWhile = true;
 
         while(runWhile){
-            int newCar = 0;
+            newCar = 0;
             System.out.println("Add a new car: ");
             newCar = key.nextInt();
             if(driveway.contains(newCar)){
@@ -54,13 +78,12 @@ public class CA3_Question1
         }
     }
 
-    public static void removeCar(){
-        Stack<Integer> driveway = new Stack<Integer>();
+
+    public static void removeCar(int removeCar){
         Scanner key = new Scanner(System.in);
         boolean runWhile = true;
 
         while(runWhile){
-            int removeCar = 0;
             System.out.println("Add a new car: ");
             removeCar = key.nextInt();
             if(driveway.contains(removeCar)){
@@ -77,6 +100,8 @@ public class CA3_Question1
     public static void lifoProblem(int removeCar){
         int simulationCommand = 0;
 
+        removeCar= removeCar*-1;
+
         if(driveway.pop() == removeCar){
             System.out.println("Car has been removed");
             simulationCommand =  removeCar*-1;
@@ -92,8 +117,5 @@ public class CA3_Question1
 
             }
         }
-
     }
-
-
 }
