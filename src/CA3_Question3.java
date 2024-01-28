0b94182fd52f5817477e8a0f1f278b61bbc2cf0c
NoahKrobot <D00259927@student.dkit.ie>
2024-01-28 16:16:20 +0000

    Q3 - empty lines skipped solved

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *  Name:
@@ -9,24 +9,37 @@ import java.util.Scanner;
 
 public class CA3_Question3
 {
-    public static void readFile(String fileName)
-    {
+    public static void readFile(String fileName) throws FileNotFoundException {
 
         File file = new File(fileName);
-        Scanner key = new Scanner(fileName);
+        Scanner key = new Scanner(file);
         key.useDelimiter("[^A-Za-z0-9_]+");
 
         int lineNumb= 0;
         String singleLine = "";
 
+
+        Map< Integer, String> numberedLinesMap;
+        numberedLinesMap = new TreeMap< Integer, String>();
+        //I'm using a TreeMap, so I can order the lines later
+        //  -> Used a hashMap at first, but it doesn't sort line by numbers and it looks odd.
+
         while (key.hasNextLine()) {
-            singleLine = key.nextLine();
+            singleLine = key.nextLine().trim();
             lineNumb++;
-            System.out.println(lineNumb+": "+singleLine);
+
+            if (!singleLine.isEmpty()) { //I added this so empty lines are skipped.
+                numberedLinesMap.put(lineNumb, singleLine);
+            }
         }
 
+        Set<Integer> keySet = numberedLinesMap.keySet();
 
 
+        for (Integer oneKey : keySet) {
+                String value = numberedLinesMap.get(oneKey);
+                System.out.println("Line " + oneKey + ": " +  value+ "\n");
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
