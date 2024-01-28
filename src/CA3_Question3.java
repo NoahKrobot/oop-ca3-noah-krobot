1773f0c4c356a410f950c0b7b8dcc9e09dfe477a
NoahKrobot <D00259927@student.dkit.ie>
2024-01-28 15:25:38 +0000

    Q3 - trying to output lines while modifying the readFile

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,4 +1,7 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *  Name:
  *  Class Group:
@@ -8,6 +11,21 @@ public class CA3_Question3
 {
     public static void readFile(String fileName)
     {
+
+        File file = new File(fileName);
+        Scanner key = new Scanner(fileName);
+        key.useDelimiter("[^A-Za-z0-9_]+");
+
+        int lineNumb= 0;
+        String singleLine = "";
+
+        while (key.hasNextLine()) {
+            singleLine = key.nextLine();
+            lineNumb++;
+            System.out.println(lineNumb+": "+singleLine);
+        }
+
+
 
     }
 
