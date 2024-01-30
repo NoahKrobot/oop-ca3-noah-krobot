7032225b8633acf0eea0796efe998e972e84ad9e
NoahKrobot <D00259927@student.dkit.ie>
2024-01-30 23:09:45 +0000

    Q5 - valid input and flight number solved

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,3 +1,8 @@
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 /**
  *  Name: Noah Krobot
  *  Class Group: SD2B
@@ -6,9 +11,58 @@
 
 public class CA3_Question5
 {
+    static Scanner key = new Scanner(System.in);
+
+    static Queue<String> takeoffQueue = new LinkedList<>();
+    static Queue<String> landingQueue = new LinkedList<>();
 
     public static void main(String[] args)
     {
+//        System.out.println("test");
 
+        String singleCommand = "";
+        String flightNumber = "";
+        System.out.println("Enter the command: ");
+
+        singleCommand = key.nextLine();
+        if(singleCommand.equals("takeoff")){
+            flightNumber = enterFlight();
+            System.out.println(flightNumber);
+        }
+
+//        else if(singleCommand.equals("land"){
+//
+//
+//    }else if(singleCommand.equals("next"){
+//
+//    }else if(singleCommand.equals("quit"){
+//
+//    }
+    }
+
+    public static String enterFlight(){
+        System.out.println("Enter a flight number: ");
+        int flightNumber = 0;
+        flightNumber = validInput();
+        String fullFlightName = "Flight-";
+        fullFlightName += flightNumber;
+        return fullFlightName;
+    }
+
+    public static int validInput(){
+        int number = 0;
+        boolean runWhile = true;
+
+        while(runWhile){
+            Scanner keyValid = new Scanner(System.in);
+
+            if(keyValid.hasNextInt()){
+                number = keyValid.nextInt();
+                runWhile=false;
+            }else{
+                System.out.println("Number has to be integer value.");
+            }
+        }
+        return number;
     }
 }
