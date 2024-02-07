a39b8d0967bc9bd66f1902fcf1e379c896028055
NoahKrobot <D00259927@student.dkit.ie>
2024-02-07 23:39:16 +0000

    Q8 - numbers go to number stacks

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -1,8 +1,13 @@
 //Tutorials:
     //get the ascii codes:
         //https://www.baeldung.com/java-character-ascii-value
+    //ascii table
+        //https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+    //Deque
+        //https://docs.oracle.com/javase/8/docs/api/java/util/Deque.html
 
-
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -31,11 +36,35 @@ public class CA3_Question8 {
         Stack<Integer> numberStack = new Stack<>();
         Stack<Integer> operationStack = new Stack<>();
 
-//        String currentCharacter = "";
+            String currentCharacterString = "";
+           char currentCharacter = 0;
+           boolean lastCharIsNumber= false;
+           String combinedNumber = "";
         for(int i=0; i<equation.length();i++){
-            char currentCharacter = equation.charAt(i);
+            currentCharacter = equation.charAt(i);
+            currentCharacterString = String.valueOf(currentCharacter);
+
             int asciiValue = (int) currentCharacter;
-            System.out.println("current ascii " + asciiValue);
+//            System.out.println("current ascii " + asciiValue);
+
+            if (Character.isDigit(currentCharacter)) {
+                if(lastCharIsNumber){
+                    combinedNumber = String.valueOf(numberStack.pop()+currentCharacterString);
+                    numberStack.push(Integer.valueOf(combinedNumber));
+                }else{
+                    numberStack.push(Integer.valueOf(currentCharacterString));
+                }
+                lastCharIsNumber = true;
+            }
+
+//            Deque<Integer> tempStack = new ArrayDeque<>(numberStack);
+//
+//            while (!tempStack.isEmpty()) {
+//                System.out.println(tempStack.pop());
+//            }
+
+
+
         }
 
 
