bb6af5b1aa3629a8536cc35268ef9dd7a972074d
NoahKrobot <D00259927@student.dkit.ie>
2024-02-07 23:18:34 +0000

    Q8 - ascii values for each character can be seen

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -1,3 +1,8 @@
+//Tutorials:
+    //get the ascii codes:
+        //https://www.baeldung.com/java-character-ascii-value
+
+
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -21,9 +26,19 @@ public class CA3_Question8 {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter equation");
         equation = in.nextLine().trim();
-        System.out.println("Equation: " + equation);
+
 
         Stack<Integer> numberStack = new Stack<>();
+        Stack<Integer> operationStack = new Stack<>();
+
+//        String currentCharacter = "";
+        for(int i=0; i<equation.length();i++){
+            char currentCharacter = equation.charAt(i);
+            int asciiValue = (int) currentCharacter;
+            System.out.println("current ascii " + asciiValue);
+        }
+
+
 
 
 
